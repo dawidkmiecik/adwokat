c02fb00a038369ff9b9562da6e5d1b9f985e79d0
dawidkmiecik <dawidkmiecik@vp.pl>
2015-12-19 19:06:58 +0100

    Adwokat Wersja 1

---
+++ include/main.php
@@ -0,0 +1,141 @@
+<main class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<article class="thumbnail">
+						<img src="img/4.jpg" class="thumbnailImg" alt="...">
+						<header class="caption">
+							<h3>Prawo karne</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt incidunt ducimus excepturi.</p>
+							<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+      					</header>
+    				</article>
+				</div><!-- col-md-4 -->
+				<div class="col-md-4">
+					<article class="thumbnail">
+						<img src="img/4.jpg" class="thumbnailImg" alt="...">
+						<header class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, dolores fugit tempora.</p>
+							<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+      					</header>
+    				</article>
+				</div><!-- col-md-4 -->
+				<div class="col-md-4">
+					<article class="thumbnail">
+						<img src="img/4.jpg" class="thumbnailImg" alt="...">
+						<header class="caption">
+							<h3>Prawo gospodarcze</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quo, id culpa.</p>
+							<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+      					</header>
+    				</article>
+				</div><!-- col-md-4 -->
+			</div><!-- row -->
+
+			<div class="row">
+				<div class="col-md-4">
+					<article class="thumbnail">
+						<img src="img/4.jpg" class="thumbnailImg" alt="...">
+						<header class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, molestiae fugiat a?</p>
+							<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+      					</header>
+    				</article>
+				</div><!-- col-md-4 -->
+				<div class="col-md-4">
+					<article class="thumbnail">
+						<img class="thumbnailImg" src="img/4.jpg" alt="...">
+						<header class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ullam, tempora expedita?</p>
+							<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+      					</header>
+    				</article>
+				</div><!-- col-md-4 -->
+				<div class="col-md-4">
+					<article class="thumbnail text-center">
+							<div class="dupa2 dupa5 text-center" style="background-color: #EA2E0;">
+						<i class="fa fa-home fa-2x  text-center"></i>
+					</div>
+						<header class="caption">
+							<h3>Thumbnail label</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quod optio a?</p>
+							<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+      					</header>
+    				</article>
+				</div><!-- col-md-4 -->
+			</div><!-- row -->
+		</main>
+		<div class="container">
+			<div class="row offer">
+				<div class="col-md-4">
+					<div class="dupa2 pull-left">
+						<i class="fa fa-balance-scale fa-2x"></i>
+					</div>
+					
+					<h3 class="pull-left dupa">Prawo karne</h3>
+					<div class="clearfix"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos, aut provident quibusdam fugiat illum accusantium dolores tempore officia praesentium!
+					<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+					</p>
+				</div>
+				<div class="col-md-4">
+					<div class="dupa2 dupa3 pull-left" style="background-color: #EA2E0;">
+						<i class="fa fa-university fa-2x"></i>
+					</div>
+					
+					<h3 class="pull-left dupa">Prawo gospodarcze</h3>
+					<div class="clearfix"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos, aut provident quibusdam fugiat illum accusantium dolores tempore officia praesentium!
+					<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+					</p>
+				</div>
+				<div class="col-md-4">
+					<div class="dupa2 dupa3 pull-left" style="background-color: #EA2E0;">
+						<i class="fa fa-home fa-2x"></i>
+					</div>
+					
+					<h3 class="pull-left dupa">Prawo kanoniczne</h3>
+					<div class="clearfix"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos, aut provident quibusdam fugiat illum accusantium dolores tempore officia praesentium!
+					<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+										<div class="dupa2 dupa3 pull-left" style="background-color: #EA2E0;">
+						<i class="fa fa-home fa-2x"></i>
+					</div>
+					
+					<h3 class="pull-left dupa">Prawo kanoniczne</h3>
+					<div class="clearfix"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos, aut provident quibusdam fugiat illum accusantium dolores tempore officia praesentium!
+					<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+					</p>
+				</div>
+				<div class="col-md-4">
+										<div class="dupa2 dupa3 pull-left" style="background-color: #EA2E0;">
+						<i class="fa fa-home fa-2x"></i>
+					</div>
+					
+					<h3 class="pull-left dupa">Prawo kanoniczne</h3>
+					<div class="clearfix"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos, aut provident quibusdam fugiat illum accusantium dolores tempore officia praesentium!
+					<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+					</p>
+				</div>
+				<div class="col-md-4">
+										<div class="dupa2 pull-left" style="background-color: #EA2E0;">
+						<i class="fa fa-home fa-2x"></i>
+					</div>
+					
+					<h3 class="pull-left dupa">Prawo kanoniczne</h3>
+					<div class="clearfix"></div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eos, aut provident quibusdam fugiat illum accusantium dolores tempore officia praesentium!
+					<p><a href="#" class="btn btn-primary" role="button">Zobacz więcej</a></p>
+					</p>
+				</div>
+			</div>
+		</div>
---
+++ include/service.php
@@ -0,0 +1,196 @@
+<section id="service">
+	<div class="container">
+		<div class="row">
+			<div class="serviceBoreder">
+			<h3>
+				Zakres usług
+				<span></span>
+			</h3>
+			</div>
+			<div class="col-md-12">
+				<p>
+					Kancelaria świadczy pomoc prawną w zakresie prawa: karnego, rodzinnego, cywilnego, pracy, administracyjnego i gospodarczego. 
+				</p>
+			</div>
+			<div class="col-md-9 serviceContent serviceContent">
+				<h4>
+					Prawo karne
+				</h4>	
+				<p>
+					prowadzi obrony karne oraz reprezentuje pokrzywdzonych jako oskarżycieli posiłkowych czy też powodów cywilnych. W ramach tej gałęzi prawa reprezentuje klientów na każdym etapie postępowania karnego- tj. postępowania przygotowawczego , procesu przed sądem I instancji, postępowania apelacyjnego, a w szczególnych przypadkach także kasacyjnego oraz postępowania wykonawczego. Praktyka i doświadczenie zawodowe pokazują, że obecność obrońcy  lub pełnomocnika wskazana jest już na jak najwcześniejszym etapie postępowania przygotowawczego prowadzonego przez prokuraturę. Dlatego w postępowaniu przygotowawczym uczestniczy we wszystkich czynnościach procesowych, w których dopuszczalna jest obecność obrońcy lub pełnomocnika. Tutaj częstokroć następują zdarzenia procesowe, które nadają kierunek dalszemu biegowi sprawy, tzn. decydują o określonej treści zarzutów (lub dalej aktu oskarżenia) bądź prowadzą do umorzenia sprawy (bez formułowania aktu oskarżenia przez prokuraturę).
+				</p>
+			</div>
+			<div class="col-md-3">
+				<div class="serviceImgTwo">
+						
+				</div>		
+			</div>
+
+			<div class="col-md-12 serviceContent">
+				<h4>
+					Prawo rodzinne
+				</h4>
+				<p>
+					Sprawy rodzinne są szczególną kategorią spraw ze względu na duży poziom emocji, jakie bardzo często udzielają się stronom postępowania. W takiej sytuacji reprezentowanie przez zawodowego pełnomocnika niewątpliwie wpływa pozytywnie na jakość, sprawność i skuteczność postępowania. 
+					W szczególności pomocne jest chłodne spojrzenie i wyzbyta emocji ocena sytuacji, ustalenie odpowiedniej strategii procesowej oraz umiejętność reakcji na niespodziewane działania procesowe drugiej strony. Głównymi polami działalności w zakresie prawa rodzinnego są:
+					<ul class="listStyle">
+						<li>
+							<p>
+								sprawy rozwodowe oraz o separację (rozwód z orzeczeniem o winie, rozwód bez orzekania o winie, wniosek o eksmisję współmałżonka, etc.),
+							</p>
+						</li>
+						<li>
+							<p>
+								sprawy o podział majątku wspólnego (ustalenie nierównych udziałów w majątku wspólnym małżonków, rozliczanie nakładów, etc.),	
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								sprawy o alimenty oraz uchylenie obowiązku alimentacyjnego (alimenty na dziecko, alimenty na współmałżonka, etc.),
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								sprawy o ustalenie czy zaprzeczenie ojcostwa,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								sprawy dotyczące ustalenia kontaktów z dzieckiem,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								sprawy dotyczące władzy rodzicielskiej,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								sprawy o ubezwłasnowolnienie.
+							</p>
+							
+						</li>
+					</ul>
+				</p>
+			</div><!-- col-md-12 -->
+			
+			<div class="col-md-3">
+				<div class="serviceImgTwo">
+						
+				</div>	
+			</div>
+
+			<div class="col-md-9 serviceContent">
+				<h4>
+					Prawo cywilne
+				</h4>	
+				<p>
+					Kancelaria świadczy pomoc prawną w zakresie prawa cywilnego na przykład w sprawach: o zapłatę, dot. nieruchomości, dot. umów, odszkodowawczych, o ubezwłasnowolnienie, o ochronę dóbr osobistych, spadkowych  itp.
+					<br />
+					Śmierć osoby bliskiej na gruncie prawa cywilnego powoduje konieczność uregulowania przez rodzinę szeregu kwestii prawnych związanych z majątkiem pozostawionym przez zmarłego. Pomoc zawodowego pełnomocnika pozwala w takiej sytuacji sprawnie i skutecznie rozwiązać problemy prawne i kwestie sporne, które częstokroć są skomplikowane. Do takich spraw należą, w szczególności:
+
+					<ul class="listStyle">
+						<li>
+							<p>
+								sprawach o stwierdzenie nabycia spadku, dział spadku, zabezpieczenie spadku,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								przyjęcie bądź odrzucenie spadku, unieważnienie testamentu,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								wydziedziczaniem osób uprawnionych do nabycia spadku z mocy ustawy,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								dochodzeniem zachowku,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								podważaniem fałszywych testamentów.
+							</p>
+							
+						</li>
+					</ul>
+				</p>
+			</div>
+			<div class="col-md-4 serviceContent">
+				<h4>Prawo pracy</h4>
+				<p>
+					Prowadzę sprawy między innymi o:
+					<ul class="listStyle">
+						<li>
+							<p>
+								wynagrodzenie za pracę i inne świadczenia pracownicze,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								odwołania o d wypowiedzenia od pracy,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								przywrócenie do pracy i odszkodowanie itd.
+							</p>
+							
+						</li>
+					</ul>
+				</p>
+			</div>
+			<div class="col-md-4 serviceContent">
+				<h4>Prawo gospodarcze</h4>
+				<p>
+					Kancelaria prowadzi sprawy dotyczące:
+					<ul class="listStyle">
+						<li>
+							<p>
+								sporządzania i opiniowania umów,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								negocjacji  z kontrahentami,
+							</p>
+							
+						</li>
+						<li>
+							<p>
+								spraw o zapłatę oraz odszkodowanie itd
+							</p>
+							
+						</li>
+					</ul>
+				</p>
+			</div>
+			<div class="col-md-4 serviceContent">
+				<h4>Prawo administracyjne</h4>
+				<p>
+					Jest to dziedzina prawa bardzo rozległa ale najczęściej są to sprawy o : 
+					<ul class="listStyle">
+						<li><p>emeryturę,</p></li>
+						<li><p> rentę,</p></li>
+						<li><p>świadczenia rodzinne itp.</p></li>
+					</ul>
+				</p>
+			</div>
+		</div><!-- row -->
+	</div><!-- container -->
+</section>
